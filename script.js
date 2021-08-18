9f7fde1e9fc44ab8a12c983cf388cb21a6ea387b
ivan_almendra <almendraivan210814@gmail.com>
2021-08-18 20:49:12 -0300

    agregando array y iterando esta misma

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+let datoPaquetes=[]
 
 function cargaPaquete() {
 
@@ -33,19 +33,22 @@ function cargaPaquete() {
     }
 
     /*-------------------APLICANDO OBJETO--------------------- */
+
     let parsePeso = parseFloat(pesoPaquete)
     let parseNumero = parseFloat(nPaquete)
     let parseDistancia = parseFloat(distancia)
     let parseTitular = titular.toUpperCase()
 
-    let datosPaquete={
-        peso:parsePeso,
-        numberoPaquete:parseNumero,
-        distanciaPaquete:parseDistancia,
-        titularPaquete:parseTitular
+    function Paquete(parsePeso,parseNumero,parseDistancia,parseTitular){
+        this.parsePeso = parsePeso,
+        this.parseNumero = parseNumero,
+        this.parseDistancia = parseDistancia,
+        this.parseTitular = parseTitular
     }
 
-    console.log(datosPaquete)
+    datoPaquetes.push(new Paquete(parsePeso,parseNumero,parseDistancia,parseTitular))
+    console.log(datoPaquetes)
+
     /*---------------------------------------- */
 
 }
@@ -67,27 +70,30 @@ function distanciaKm(distancia, tarifa1) {
 /*                               */
 
  
+let listaPaquete=[
+    {id:5489, direccion:"rio desaguadero",numero:5014},
+    {id:4545, direccion:"avenida mitre",numero:4525},
+    {id:8989, direccion:"rio gualeguay",numero:258}
+]
+
 buscarPaquete=()=>{
     let pesoPaquete = document.getElementById('pesoPaquete').value
     let nPaquete = document.getElementById('nPaquete').value
     let distancia = document.getElementById('distancia').value
     let titular = document.getElementById('titular').value
-    let password = document.getElementById('password').value
 
     let parsePeso = parseFloat(pesoPaquete)
     let parseNumero = parseFloat(nPaquete)
     let parseDistancia = parseFloat(distancia)
     let parseTitular = titular.toUpperCase()
 
-    function Paquete(parsePeso,parseNumero,parseDistancia,parseTitular){
-        this.parsePeso = parsePeso,
-        this.parseNumero = parseNumero,
-        this.parseDistancia = parseDistancia,
-        this.parseTitular = parseTitular
+    for(const paquetes of listaPaquete){
+        if(paquetes.id == parseNumero){
+            alert("Este paquete ya se encuentra \n cargada en la base de datos")
+        }
     }
 
-    const prueba1 = new Paquete(parsePeso,parseNumero,parseDistancia,parseTitular)
-    console.log(prueba1)
-    console.log(parsePeso,parseNumero,parseDistancia,parseTitular)
+    
+
 
 }
