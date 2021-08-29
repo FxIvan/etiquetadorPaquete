dcc73217a8ccfb31c39646b0cdb1f79444f6fa5d
ivan_almendra <almendraivan210814@gmail.com>
2021-08-29 18:56:58 -0300

    agregando eventos

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let datoPaquetes=[]
+let datoPaquetes = []
 
-let pesoPaquete = document.getElementById('pesoPaquete').value
-let nPaquete = document.getElementById('nPaquete').value
-let distancia = document.getElementById('distancia').value
-let titular = document.getElementById('titular').value
-let password = document.getElementById('password').value
+let nPaquete = document.getElementById('nPaquete')
+let distancia = document.getElementById('distancia')
+let titular = document.getElementById('titular')
+let direccion = document.getElementById('direccion')
 
 
+nPaquete.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value
+    document.getElementById('sPaquete').innerHTML = valorInput
+})
+
+
+titular.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value
+    document.getElementById('sSalida').innerHTML = valorInput
+})
+
+direccion.addEventListener('keyup', (e) => {
+    let valorInput = e.target.value
+    document.getElementById('direccionSalida').innerHTML = valorInput
+})
 
 function cargaPaquete() {
 
@@ -15,6 +29,7 @@ function cargaPaquete() {
     let distancia = document.getElementById('distancia').value
     let titular = document.getElementById('titular').value
     let password = document.getElementById('password').value
+    let direccion = document.getElementById('direccion').value
 
     let tarifa1 = 50
     let tarifa2 = 100
@@ -45,71 +60,70 @@ function cargaPaquete() {
     let parsePeso = parseFloat(pesoPaquete)
     let parseNumero = parseFloat(nPaquete)
     let parseDistancia = parseFloat(distancia)
+    let parseDireccion =  direccion.toUpperCase()
     let parseTitular = titular.toUpperCase()
 
-/**Aca estaba la funcion Paquete **/
+    /**Aca estaba la funcion Paquete **/
 
-    datoPaquetes.push(new Paquete(parsePeso,parseNumero,parseDistancia,parseTitular))
+    datoPaquetes.push(new Paquete(parsePeso, parseNumero, parseDistancia, parseTitular,parseDireccion))
     console.log(datoPaquetes)
 
     /*---------------------------------------- */
 
 }
-    function Paquete(parsePeso,parseNumero,parseDistancia,parseTitular){
-        this.parsePeso = parsePeso,
+
+function Paquete(parsePeso, parseNumero, parseDistancia, parseTitular,parseDireccion) {
+    this.parsePeso = parsePeso,
         this.parseNumero = parseNumero,
         this.parseDistancia = parseDistancia,
-        this.parseTitular = parseTitular
-    }
+        this.parseTitular = parseTitular,
+        this.parseDireccion = parseDireccion
+}
 
 function distanciaKm(distancia, tarifa1) {
     let resultadoParcial = distancia * 15
     let resultadoTotal = resultadoParcial + tarifa1
 
     let precioFinal = document.getElementById('precioFinal')
-    precioFinal.innerHTML = resultadoTotal
+
+    precioEnvio.innerHTML = resultadoTotal
     alert(resultadoTotal)
 }
-/*
-        betaInput.addEventListener('keyup', (e)=>{
-            let valorInput = e.target.value
-            document.getElementById('betaSalida').textContent = valorInput
-        })
-        */
-/*                               */
 
- 
-let listaPaquete=[
-    {id:5489, direccion:"rio desaguadero",numero:5014},
-    {id:4545, direccion:"avenida mitre",numero:4525},
-    {id:8989, direccion:"rio gualeguay",numero:258}
+
+
+let listaPaquete = [{
+        id: 5489,
+        direccion: "rio desaguadero",
+        numero: 5014
+    },
+    {
+        id: 4545,
+        direccion: "avenida mitre",
+        numero: 4525
+    },
+    {
+        id: 8989,
+        direccion: "rio gualeguay",
+        numero: 258
+    }
 ]
 
-buscarPaquete=()=>{
-    let pesoPaquete = document.getElementById('pesoPaquete').value
+buscarPaquete = () => {
     let nPaquete = document.getElementById('nPaquete').value
-    let distancia = document.getElementById('distancia').value
-    let titular = document.getElementById('titular').value
-
-    let parsePeso = parseFloat(pesoPaquete)
     let parseNumero = parseFloat(nPaquete)
-    let parseDistancia = parseFloat(distancia)
-    let parseTitular = titular.toUpperCase()
 
-    for(const paquetes of listaPaquete){
-        if(paquetes.id == parseNumero){
+    for (const paquetes of listaPaquete) {
+        if (paquetes.id == parseNumero) {
             alert("Este paquete ya se encuentra \n cargada en la base de datos")
             let contenedor = document.createElement('div')
 
-            contenedor.innerHTML=`<p>Numero de Paquete: ${paquetes.id}</p>
+            contenedor.innerHTML = `<p>Numero de Paquete: ${paquetes.id}</p>
                               <p>Direccion : ${paquetes.direccion}</p>
                               <p>Numero: ${paquetes.numero}</p>`
             document.body.appendChild(contenedor)
             contenedor.classList.add("contenedorMuestraPaquete")
         }
     }
-
-    
-
 
 }
